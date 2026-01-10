83ae59f15d503ccbcb4ceeb5e0ce2b8cb40698f9
wellitonalvesgit <ascartasdepailoo@gmail.com>
2026-01-10 01:08:37 -0300

    Set final AMX A-1 image for Login Page

--- client/src/pages/auth/LoginPage.tsx
+++ client/src/pages/auth/LoginPage.tsx
@@ -62,9 +62,9 @@ export function LoginPage() {
 
       {/* A-1 AMX Fighter Jet Decor */}
       <img
-        src="/a1_4k.jpg"
+        src="/a1_final.jpg"
         alt="AMX A-1"
-        className="absolute bottom-0 left-1/2 w-[1600px] max-w-none -translate-x-1/2 opacity-60 pointer-events-none z-0 lg:opacity-80"
+        className="absolute bottom-0 left-1/2 w-[1400px] max-w-none -translate-x-1/2 translate-y-[10%] opacity-80 pointer-events-none z-0 mix-blend-screen lg:opacity-100"
         style={{ maskImage: 'linear-gradient(to top, black 50%, transparent 100%)', WebkitMaskImage: 'linear-gradient(to top, black 50%, transparent 100%)' }}
       />
     </div >
